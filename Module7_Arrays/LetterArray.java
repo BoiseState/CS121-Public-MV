9429290b3a9a848de1ad2fb0095b9aeb82609519
MasonVail <masonvail@boisestate.edu>
2026-04-09 14:56:36 -0600

    improved letter array

--- Module7_Arrays/LetterArray.java
+++ Module7_Arrays/LetterArray.java
@@ -1,29 +1,71 @@
 /**
  * Demo initializing an array of all 26 letters.
+ * 
  * @author mvail
  */
 public class LetterArray {
     public static void main(String[] args) {
-        //could use an initializer list, but will get long
-        //char[] letter = {'a', 'b', 'c', 'z'};
+        arrayUsingInitializerList();
 
-        //could initialize all indexes explicitly, but ewww -
-        // even more tedious than using initializer list
+        arrayOneIndexAtATime();
+
+        arrayUsingCharMath();
+    }
+
+    /** Print nicely formatted array with given label. */
+    private static void printArray(char[] array, String label) {
+        System.out.println(label);
+        for (char c : array) {
+            System.out.printf("%2s", c);
+        }
+        System.out.println();
+    }
+
+    /** Populate array using an initializer list. */
+    private static void arrayUsingInitializerList() {
+        char[] letter = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                          'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        printArray(letter, "Array from initializer list:");
+    }
+
+    /** Populate array one index at a time. */
+    private static void arrayOneIndexAtATime() {
         char[] letter = new char[26];
-        // letter[0] = 'a';
-        // letter[1] = 'b';
-        // letter[2] = 'c';
-        // letter[25] = 'z';
+        letter[0] = 'a';
+        letter[1] = 'b';
+        letter[2] = 'c';
+        letter[3] = 'd';
+        letter[4] = 'e';
+        letter[5] = 'f';
+        letter[6] = 'g';
+        letter[7] = 'h';
+        letter[8] = 'i';
+        letter[9] = 'j';
+        letter[10] = 'k';
+        letter[11] = 'l';
+        letter[12] = 'm';
+        letter[13] = 'n';
+        letter[14] = 'o';
+        letter[15] = 'p';
+        letter[16] = 'q';
+        letter[17] = 'r';
+        letter[18] = 's';
+        letter[19] = 't';
+        letter[20] = 'u';
+        letter[21] = 'v';
+        letter[22] = 'w';
+        letter[23] = 'x';
+        letter[24] = 'y';
+        letter[25] = 'z';
+        printArray(letter, "Array One Index at a Time:");
+    }
 
-        //use a list and character math - awesome
+    /** Populate array using character math. */
+    private static void arrayUsingCharMath() {
+        char[] letter = new char[26];
         for (int i = 0; i < 26; i++) {
-            letter[i] = (char)('a' + i);
+            letter[i] = (char) ('a' + i);
         }
-
-        //can use for-each loops to access (but not change)
-        // arrays - useful for printing
-        for (char c : letter) {
-            System.out.println(c);
-        }
+        printArray(letter, "Array Using Character Math:");
     }
 }
